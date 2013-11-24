18713da42e6e15c35fd288adf9fcaf9cbd53df35
iumehara <iumehara10@gmail.com>
2013-11-24 14:17:35 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ChattingApp::Application.config.secret_key_base = 'e44ca5e94f018e6c794655a49f63c673dd7e86735f353a74cb92ba4656841e4788ec9179bb0ab36a727418854ff7cfc462430d1cabb1cb0e95aa3cb55401ed09'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ChattingApp::Application.config.session_store :cookie_store, key: '_ChattingApp_session'
